cca9ab31932ba6429732906f88260adb83526741
martaskarbek <marta@skarbek.name>
2021-02-11 21:26:26 +0100

    add main folders

---
+++ blogMe.Data/Context/BaseDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace blogMe.Data.Context
+{
+    public class BaseDBContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public BaseDBContext(DbContextOptions<BaseDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
